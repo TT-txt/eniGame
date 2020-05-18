a3dca7440e9cbdde622af904f9719207cc8c9199
TT-txt <theodore.martin@student.yncrea.fr>
2020-05-18 10:24:37 +0200

    in order to avoid merge

--- web/js/maps.js
+++ web/js/maps.js
@@ -19,7 +19,7 @@ const startMap0 = new map(
 const startMap1 = new map(
     [new THREE.Vector3(-5, 5, -5), new THREE.Vector3(1, 1, 3), new THREE.Vector3(1, 1, 4), new THREE.Vector3(3, 2,  2),new THREE.Vector3(3, 1,  2)],
     new THREE.Vector3(5, 0, 5),
-    [new trap(1, new THREE.Vector3(1, 2, 3), false, "e")],
+    [new trap(1, new THREE.Vector3(1, 2, 3), false, 'e')],
     [new logic(0, new THREE.Vector3(4, 1, 4), 0, false), new logic(1, new THREE.Vector3(), 0, new THREE.Vector3(2, 1, 1))],
     false,
     null,
