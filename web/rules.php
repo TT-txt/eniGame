11d0f9064ce3dcd9c11f97222be70603dc10abf2
Marie Pivette <marie.pivette@student.yncrea.fr>
2020-05-19 16:47:06 +0200

    finish theTeam + modif rules

--- web/rules.php
+++ web/rules.php
@@ -6,7 +6,20 @@
 <body>
 	<?php include("include/nav.php"); ?>
 	<main>
-		Rules
+	<div class="container" style="margin-top:30px">
+		<div class="row">
+			<div class="col-sm-4">
+				<img class="card-img-top" src="https://www.codeur.com/blog/wp-content/uploads/2018/12/codeur-developpeur-web-700x525.jpg" alt="Louis Ducrocq" style="height: 200px; width: 100%; display: block;" data-holder-rendered="true">
+			</div>
+			<div class="col-sm-8">
+				<h2>TITLE HEADING</h2>
+				<h5>Title description, Dec 7, 2017</h5>
+				<p>Some text..</p>
+				<p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+				<br>
+			</div>
+		</div>
+	</div>
 	</main>
 	<?php include("include/footer.php"); ?>
 </body>
--- web/theTeam.php
+++ web/theTeam.php
@@ -6,7 +6,68 @@
 <body>
 	<?php include("include/nav.php"); ?>
 	<main>
-		theTeam
+		<section class="px-3 py-3  mx-auto text-center">
+			<div class="container">
+				<h1>The Team</h1>
+				<p class="lead text-muted">Hello there, here is our Team. Six (more or less, depending of the weather) persons. <br/>If you want to know more, read the description below. </p>
+			</div>
+		</section>
+
+		<div class="container">
+			<div class="row">
+				<div class="col-md-4">
+					<div class="card mb-4 shadow-sm">
+						<img class="card-img-top" src="img/louisd.png" alt="Louis Ducrocq" style="height: 200px; width: 100%; display: block;" data-holder-rendered="true">
+						<div class="card-body">
+							<p class="card-text"><center><b>Louis D</b>. EL PLANNOR. </center>Don't even think to drink or laugh IF IT'S NOT WRITTEN IN THE PLANNING! </p>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-4">
+					<div class="card mb-4 shadow-sm">
+						<img class="card-img-top" src="img/noé.png" alt="Noé Klopocki" style="height: 200px; width: 100%; display: block;" data-holder-rendered="true">
+						<div class="card-body">
+							<p class="card-text"><center><b>Noé</b> tries to reconnect. </center> Working in a bin. <br/> Yes. Literally. </p>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-4">
+					<div class="card mb-4 shadow-sm">
+						<img class="card-img-top" src="img/louism.png" alt="Louis Manouvriez" style="height: 200px; width: 100%; display: block;" data-holder-rendered="true">
+						<div class="card-body">
+							<p class="card-text"><center><b>Louis M</b>...</center> Louis M ?! Wait ! Was there any Louis M in the group ??</p>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="row">
+				<div class="col-md-4">
+					<div class="card mb-4 shadow-sm">
+						<img class="card-img-top" src="img/hugo.png" alt="Hugo Martel" style="height: 200px; width: 100%; display: block;" data-holder-rendered="true">
+						<div class="card-body">
+							<p class="card-text"><center><b>Hugo</b>, maybe the best.</center>You can call him "Mister Push". <br/> Guess why ? </p>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-4">
+					<div class="card mb-4 shadow-sm">
+						<img class="card-img-top" src="img/tt.png" alt="Théodore Martin" style="height: 200px; width: 100%; display: block;" data-holder-rendered="true">
+						<div class="card-body">
+							<p class="card-text"><center><b>Théodore</b>, alias TT. </center>You can ask him everything. The only thing is that he won't answer.</p>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-4">
+					<div class="card mb-4 shadow-sm">
+						<img class="card-img-top" src="img/marie.png" alt="Marie Pivette" style="height: 200px; width: 100%; display: block;" data-holder-rendered="true">
+						<div class="card-body">
+							<p class="card-text"><center><b>Marie</b>, the feminine touch.</center>Always nice to work with her, except if you work with her... oO</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
 	</main>
 	<?php include("include/footer.php"); ?>
 </body>
