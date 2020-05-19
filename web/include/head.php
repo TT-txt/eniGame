904ab3eb115b4f16c933ffae001affa91c88a573
Marie Pivette <58333036+mariePivette@users.noreply.github.com>
2020-05-19 11:28:15 +0200

    style opti

--- web/include/head.php
+++ web/include/head.php
@@ -47,6 +47,61 @@ function test_input($data)
 		main {
 			height: 900px;
 		}
+		
+		body {
+			height: 100%;
+			width: 100%;
+			padding: 0;
+			margin: 0;
+			background-color: #e0e0e0;
+		}
+
+		.btn {
+			margin: 10px;
+		}
+
+		.btn-success {
+			background-color: #6351ce !important;
+			border-color: #6351ce !important;
+		}
+
+		.btn-success:hover,
+		.btn-success:active,
+		.btn-success:visited {
+			border-color: #A9A9A9 !important;
+		}
+
+		.btn-success.focus, .btn-success:focus {
+			box-shadow: 0 0 0 0.2rem rgba(0,123,255,.25);
+		}
+
+		.btn-success:not(:disabled):not(.disabled).active:focus, .btn-success:not(:disabled):not(.disabled):active:focus, .show>.btn-success.dropdown-toggle:focus {
+			box-shadow: 0 0 0 0.2rem rgba(0,123,255,.25);
+		}
+
+		a.text-light:focus, a.text-light:hover {
+			color: #cbd3da!important;
+		}
+
+		.btn-success:hover, .btn-success:active, .btn-success:visited {
+			border-color: #A9A9A9 !important;
+		}
+
+		.text-autre {
+			color: #6351ce !important;
+		}
+
+		.row {
+			margin-right: 0px !important;
+		}
+
+		@font-face {
+			font-family: '8bit_wondernominal';
+			src: url('fonts/8-bit_wonder-webfont.woff2') format('woff2'),
+				url('fonts/8-bit_wonder-webfont.woff') format('woff');
+			font-weight: normal;
+			font-style: normal;
+		}
 	</style>
 	<!-- Bootstrap Js -->
 	<script src="include/bootstrap/required/jquery-3.4.1.slim.min.js"></script>
