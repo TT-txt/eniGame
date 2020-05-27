c7a453a0653fc0d0cc93b8dc598a40478915e0b1
TT-txt <theodore.martin@student.yncrea.fr>
2020-05-27 10:58:23 +0200

    table for map selector

--- web/tmp.php
+++ web/tmp.php
@@ -6,10 +6,10 @@
 <body>
     <?php include("include/nav.php"); ?>
     <!-- Button trigger modal -->
-    <button type="button" class="" data-toggle="modal" data-target="#trapsModal">Launch trap modal</button>
-    <button type="button" class="" data-toggle="modal" data-target="#pressureModal">Launch logic modal</button>
+    <!-- <button type="button" class="" data-toggle="modal" data-target="#trapsModal">Launch trap modal</button>
+    <button type="button" class="btn btn-success" data-toggle="modal" data-target="#pressureModal">Launch logic modal</button>-->
     <!--Trap Modal -->
-    <div class="modal fade bd-example-modal-sm" id="trapsModal" tabindex="-1" role="dialog" aria-labelledby="ModalCenterTitle" aria-hidden="true">
+    <!--<div class="modal fade bd-example-modal-sm" id="trapsModal" tabindex="-1" role="dialog" aria-labelledby="ModalCenterTitle" aria-hidden="true">
         <div class="modal-dialog modal-sm modal-dialog-centered" role="document">
             <div class="modal-content">
                 <div class="modal-header">
@@ -50,9 +50,9 @@
                     </div>
                 </div>
             </div>
-        </div>
+        </div>-->
         <!-- Logic modal -->
-        <div class="modal fade bd-example-modal-sm" id="pressureModal" tabindex="-1" role="dialog" aria-labelledby="ModalCenterTitle" aria-hidden="true">
+        <!--<div class="modal fade bd-example-modal-sm" id="pressureModal" tabindex="-1" role="dialog" aria-labelledby="ModalCenterTitle" aria-hidden="true">
             <div class="modal-dialog modal-sm modal-dialog-centered" role="document">
                 <div class="modal-content">
                     <div class="modal-header">
@@ -85,6 +85,42 @@
                     </div>
                 </div>
             </div>
+        </div>-->
+        <div class="container jumbotron">
+            <p class="h1">Levels</p>
+            <?php
+            include("include/connect.php");
+            //selecting everything from user purchases
+            $query = 'SELECT * FROM SEEDS;';
+            $result = mysqli_query($link, $query);
+            if($result){
+              $queryResult = mysqli_num_rows($result);
+              echo '<p class="h3">There are '.$queryResult. ' map';
+              if($queryResult > 1) echo 's';
+              echo ' online</p><br/>';
+              if($queryResult <= 0) echo '<p>yet there are no map online... Try to create your own using the editor</p>';
+              else{
+                echo '<table class="table"><thead class="thead-dark"><tr><th scope="col">level ID</th><th scope="col">Who Posted</th><th scope="col">play!</th></tr></thead><tbody>';
+                while ($row = mysqli_fetch_assoc($result)) {
+                    $furtherQuery = 'SELECT NAME FROM USERS WHERE ID="' . $row["whoposted"] . '";'; //select stuff from the stock db in order to show it
+                    $furtherResult = mysqli_query($link, $furtherQuery); 
+                    if ($furtherResult) {//checking if the query was successful
+                        $furtherQueryResult = mysqli_num_rows($furtherResult);
+                        if ($furtherQueryResult > 0) { //if we have results
+                            while ($row2 = mysqli_fetch_assoc($furtherResult)) {
+                                echo '<tr><th scope="row">' . $row['ID'] . '</th>';
+                                echo '<td> @' . $row2['NAME'] . '</td>';
+                                echo '<td style="padding:0px"><button type="button" class="btn btn-success btn-sm">PLAY</button></td>';
+                                //echo for finished ?
+                                echo '</tr>';
+                                }
+                            }
+                        }
+                    }
+                    echo '</tbody></table>';//close the table at the end
+                }
+            }
+            ?>
         </div>
         <?php include('include/footer.php'); ?>
 </body>
