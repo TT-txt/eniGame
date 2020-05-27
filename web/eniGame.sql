9b2f1347e43926ecd23f52f5a911c62950e35996
HugoMartel <hugo.martel@student.yncrea.fr>
2020-05-27 10:19:21 +0200

    Updated SQL for real

--- web/eniGame.sql
+++ web/eniGame.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.2
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: May 19, 2020 at 10:13 AM
+-- Host: 127.0.0.1
+-- Generation Time: May 27, 2020 at 10:16 AM
 -- Server version: 10.4.11-MariaDB
--- PHP Version: 7.4.1
+-- PHP Version: 7.4.5
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -19,16 +18,16 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `eniGame`
+-- Database: `enigame`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `MAPS`
+-- Table structure for table `maps`
 --
 
-CREATE TABLE `MAPS` (
+CREATE TABLE `maps` (
   `ID` int(11) NOT NULL,
   `content` text NOT NULL,
   `whoposted` int(11) NOT NULL,
@@ -38,23 +37,32 @@ CREATE TABLE `MAPS` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `SCORES`
+-- Table structure for table `scores`
 --
 
-CREATE TABLE `SCORES` (
+CREATE TABLE `scores` (
   `REF` int(11) NOT NULL,
   `who` int(11) NOT NULL,
   `score` bigint(20) NOT NULL,
   `date` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `scores`
+--
+
+INSERT INTO `scores` (`REF`, `who`, `score`, `date`) VALUES
+(1, 3, 5, '2020-05-27'),
+(2, 2, 5, '2020-05-27'),
+(3, 2, 5, '2020-05-27');
+
 -- --------------------------------------------------------
 
 --
--- Table structure for table `USERS`
+-- Table structure for table `users`
 --
 
-CREATE TABLE `USERS` (
+CREATE TABLE `users` (
   `ID` int(11) NOT NULL,
   `login` varchar(50) NOT NULL,
   `password` varchar(50) NOT NULL,
@@ -63,10 +71,10 @@ CREATE TABLE `USERS` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `USERS`
+-- Dumping data for table `users`
 --
 
-INSERT INTO `USERS` (`ID`, `login`, `password`, `name`, `wins`) VALUES
+INSERT INTO `users` (`ID`, `login`, `password`, `name`, `wins`) VALUES
 (1, 'admin@admin.com', 'Admin123', 'TT', 0),
 (2, 'marie.pivette@student.yncrea.fr', 'Marie123', 'Test3000', 0),
 (3, 'hugo.martel@student.yncrea.fr', 'Hugo1234', 'Gugo', 0),
@@ -80,22 +88,23 @@ INSERT INTO `USERS` (`ID`, `login`, `password`, `name`, `wins`) VALUES
 --
 
 --
--- Indexes for table `MAPS`
+-- Indexes for table `maps`
 --
-ALTER TABLE `MAPS`
+ALTER TABLE `maps`
   ADD PRIMARY KEY (`ID`),
   ADD KEY `SEEDtoUSERS` (`whoposted`);
 
 --
--- Indexes for table `SCORES`
+-- Indexes for table `scores`
 --
-ALTER TABLE `SCORES`
-  ADD PRIMARY KEY (`REF`);
+ALTER TABLE `scores`
+  ADD PRIMARY KEY (`REF`),
+  ADD KEY `SCOREStoUSERS` (`who`);
 
 --
--- Indexes for table `USERS`
+-- Indexes for table `users`
 --
-ALTER TABLE `USERS`
+ALTER TABLE `users`
   ADD PRIMARY KEY (`ID`);
 
 --
@@ -103,21 +112,21 @@ ALTER TABLE `USERS`
 --
 
 --
--- AUTO_INCREMENT for table `MAPS`
+-- AUTO_INCREMENT for table `maps`
 --
-ALTER TABLE `MAPS`
+ALTER TABLE `maps`
   MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `SCORES`
+-- AUTO_INCREMENT for table `scores`
 --
-ALTER TABLE `SCORES`
-  MODIFY `REF` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `scores`
+  MODIFY `REF` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
--- AUTO_INCREMENT for table `USERS`
+-- AUTO_INCREMENT for table `users`
 --
-ALTER TABLE `USERS`
+ALTER TABLE `users`
   MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
@@ -125,16 +134,16 @@ ALTER TABLE `USERS`
 --
 
 --
--- Constraints for table `MAPS`
+-- Constraints for table `maps`
 --
-ALTER TABLE `MAPS`
-  ADD CONSTRAINT `SEEDtoUSERS` FOREIGN KEY (`whoposted`) REFERENCES `USERS` (`ID`);
+ALTER TABLE `maps`
+  ADD CONSTRAINT `SEEDtoUSERS` FOREIGN KEY (`whoposted`) REFERENCES `users` (`ID`);
 
 --
--- Constraints for table `SCORES`
+-- Constraints for table `scores`
 --
-ALTER TABLE `SCORES`
-  ADD CONSTRAINT `SCOREStoUSERS` FOREIGN KEY (`REF`) REFERENCES `USERS` (`ID`);
+ALTER TABLE `scores`
+  ADD CONSTRAINT `SCOREStoUSERS` FOREIGN KEY (`who`) REFERENCES `users` (`ID`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
